ac10a0c9536cd8b0ccd29f55823e2ee39ff122bc
krithigau <krithigau2005@gmail.com>
2024-04-06 22:54:19 +0530

    Success

---
+++ myproj/myproj/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from myapp import views
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('surfacearea/',views.surfacearea,name="surfacearea"),
+    path('',views.surfacearea,name="surfacearea")
+]
